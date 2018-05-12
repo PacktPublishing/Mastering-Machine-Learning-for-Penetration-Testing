4f4787c438bfe1acd0106ca232e6e945db113e0f
Chiheb Chebbi <chiheb-chebbi@outlook.fr>
2018-05-12 20:02:43 +0000

    Create CNN.py

---
+++ Chapter 4/CNN.py
@@ -0,0 +1,20 @@
+import numpy
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend
+backend.set_image_dim_ordering('th')
+model = Sequential()
+model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28),
+activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
