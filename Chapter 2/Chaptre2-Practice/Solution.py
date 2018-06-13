6d73e41d709dbefd9ae98f40f1dc66a0a5d2788b
Chiheb Chebbi <chiheb-chebbi@outlook.fr>
2018-06-13 23:38:53 +0000

    Create Solution.py

---
+++ Chapter 2/Chaptre2-Practice/Solution.py
@@ -0,0 +1,78 @@
+# inspired by Abhijeet Singh classifier
+
+import os
+import numpy as np
+from collections import Counter
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.metrics import confusion_matrix
+
+def make_Dictionary(train_dir):
+    emails = [os.path.join(train_dir,f) for f in os.listdir(train_dir)]    
+    all_words = []       
+    for mail in emails:    
+        with open(mail) as m:
+            for i,line in enumerate(m):
+                if i == 2:
+                    words = line.split()
+                    all_words += words
+    
+    dictionary = Counter(all_words)
+    
+    list_to_remove = dictionary.keys()
+    for item in list_to_remove:
+        if item.isalpha() == False: 
+            del dictionary[item]
+        elif len(item) == 1:
+            del dictionary[item]
+    dictionary = dictionary.most_common(3000)
+    return dictionary
+    
+def extract_features(mail_dir): 
+    files = [os.path.join(mail_dir,fi) for fi in os.listdir(mail_dir)]
+    features_matrix = np.zeros((len(files),3000))
+    docID = 0;
+    for fil in files:
+      with open(fil) as fi:
+        for i,line in enumerate(fi):
+          if i == 2:
+            words = line.split()
+            for word in words:
+              wordID = 0
+              for i,d in enumerate(dictionary):
+                if d[0] == word:
+                  wordID = i
+                  features_matrix[docID,wordID] = words.count(word)
+        docID = docID + 1     
+    return features_matrix
+    
+# Create a dictionary of words with its frequency
+
+train_dir = 'lingspam_public\\lemm_stop\\train-mails'
+dictionary = make_Dictionary(train_dir)
+
+# Prepare feature vectors per training mail and its labels
+
+train_labels = np.zeros(702)
+train_labels[351:701] = 1
+train_matrix = extract_features(train_dir)
+
+# Training SVM and Naive bayes classifier and its variants
+
+model = LinearSVC()
+
+
+model.fit(train_matrix,train_labels)
+
+
+# Test the unseen mails for Spam
+
+test_dir = 'lingspam_public\\lemm_stop\\test-mails'
+test_matrix = extract_features(test_dir)
+test_labels = np.zeros(260)
+test_labels[130:260] = 1
+
+result = model.predict(test_matrix)
+
+print confusion_matrix(test_labels,result)
+
