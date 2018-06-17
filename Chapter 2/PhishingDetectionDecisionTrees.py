406382d6a0c43000afef4b9792dab2ba303f581b
Chiheb Chebbi <chiheb-chebbi@outlook.fr>
2018-06-17 20:05:09 +0000

    Update PhishingDetectionDecisionTrees.py

--- Chapter 2/PhishingDetectionDecisionTrees.py
+++ Chapter 2/PhishingDetectionDecisionTrees.py
@@ -1,18 +1,19 @@
-import numpy as np
-from sklearn import *
 from sklearn import tree
+from sklearn.metrics import accuracy_score
+import numpy as np
 
-Data = np.genfromtxt('dataset.csv', delimiter=',', dtype=np.int32)
-features = Data[:,:-1]
-results = Data[:, -1]
-train_features = features[:2000]
-test_features = features[2000:]
-train_results = results[:2000]
-test_results = results[2000:]
+data = np.genfromtxt('dataset.csv', delimiter=',', dtype=np.int32)
+features = data[:,:-1]
+results = data[:, -1]
 
+training_features = features[:2000]
+training_results = results[:2000]
+testing_features = features[2000:]
+testing_results = results[2000:]
 classifier = tree.DecisionTreeClassifier()
-classifier.fit(train_features, train_results)
-predictions = classifier.predict(test_results)
-accuracy = 100.0 * accuracy_score(test_results, predictions)
+classifier.fit(training_features, training_results)
+predictions = classifier.predict(testing_features)
+accuracy = 100.0 * accuracy_score(testing_results, predictions)
+
 print ("The accuracy of the Phishing detector Model with Decision Tree" + str(accuracy))
 
