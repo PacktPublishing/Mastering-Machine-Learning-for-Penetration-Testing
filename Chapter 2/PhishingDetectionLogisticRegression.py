4497495a0c13f95d8ed1fc7607272927f984d7ec
Chiheb Chebbi <chiheb-chebbi@outlook.fr>
2018-06-17 19:14:20 +0000

    Update PhishingDetectionLogisticRegression.py

--- Chapter 2/PhishingDetectionLogisticRegression.py
+++ Chapter 2/PhishingDetectionLogisticRegression.py
@@ -10,7 +10,7 @@ test_features = features[2000:]
 train_results = results[:2000]
 test_results = results[2000:]
 classifier = LogisticRegression()
-classifier.fit(train_features train_results)
+classifier.fit(train_features, train_results)
 predictions = classifier.predict(test_results)
 accuracy = 100.0 * accuracy_score(test_results, predictions)
 print ("The accuracy of the Phishing detector Model with Logistic Regression is: " + str(accuracy))
