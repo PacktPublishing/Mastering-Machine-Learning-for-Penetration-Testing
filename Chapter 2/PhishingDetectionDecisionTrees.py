dc2d717b5dbb48a8a34f6c8304afaf74c89dd2e7
Chiheb Chebbi <chiheb-chebbi@outlook.fr>
2018-06-17 20:22:08 +0000

    Update PhishingDetectionDecisionTrees.py

--- Chapter 2/PhishingDetectionDecisionTrees.py
+++ Chapter 2/PhishingDetectionDecisionTrees.py
@@ -1,19 +1,20 @@
-from sklearn import tree
-from sklearn.metrics import accuracy_score
+from sklearn import tree 
+from sklearn.metrics import accuracy_score 
 import numpy as np
 
-data = np.genfromtxt('dataset.csv', delimiter=',', dtype=np.int32)
-features = data[:,:-1]
-results = data[:, -1]
+training_data = np.genfromtxt('dataset.csv', delimiter=',', dtype=np.int32)
+inputs = training_data[:,:-1]
+outputs = training_data[:, -1]
 
-training_features = features[:2000]
-training_results = results[:2000]
-testing_features = features[2000:]
-testing_results = results[2000:]
+training_inputs = inputs[:2000] 
+training_outputs = outputs[:2000] 
+testing_inputs = inputs[2000:] 
+testing_outputs = outputs[2000:]
+
 classifier = tree.DecisionTreeClassifier()
-classifier.fit(training_features, training_results)
-predictions = classifier.predict(testing_features)
-accuracy = 100.0 * accuracy_score(testing_results, predictions)
+classifier.fit(training_inputs, training_outputs)
+predictions = classifier.predict(testing_inputs)
+accuracy = 100.0 * accuracy_score(testing_outputs, predictions)
+print ("The accuracy of your decision tree on testing data is: " + str(accuracy))
 
-print ("The accuracy of the Phishing detector Model with Decision Tree :" + str(accuracy))
 
